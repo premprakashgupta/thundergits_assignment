b13b04e1fd86011d15e9894e5c8bcece6c2a49a2
PREM PRAKASH GUPTA <72307003+premprakashgupta@users.noreply.github.com>
2025-03-06 14:51:38 +0530

    Update urls.js

--- frontend/urls.js
+++ frontend/urls.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:5000/api"
-// export const BASE_URL="https://k-and-a-assignment.onrender.com/api/employees"
+//export const BASE_URL="http://localhost:5000/api"
+ export const BASE_URL="https://thundergits-assignment.onrender.com/api"
